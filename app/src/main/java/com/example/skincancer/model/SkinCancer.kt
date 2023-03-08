d059a2422cfa19986e433d34d03b9352f0795756
Lyan-bit <72354386+Lyan-bit@users.noreply.github.com>
2023-03-08 23:16:08 +0000

    Update SkinCancer.kt

--- app/src/main/java/com/example/skincancer/model/SkinCancer.kt
+++ app/src/main/java/com/example/skincancer/model/SkinCancer.kt
@@ -1,37 +1,36 @@
 package com.example.skincancer
 
 import java.util.HashMap
-import java.util.HashSet
 
 class SkinCancer {
 
     init {
-        SkinCancer_allInstances.add(this)
+        skinCancer_allInstances.add(this)
     }
 
     companion object {
-        var SkinCancer_allInstances = ArrayList<SkinCancer>()
+        var skinCancer_allInstances = ArrayList<SkinCancer>()
         fun createSkinCancer(): SkinCancer {
             return SkinCancer()
         }
         
-        var SkinCancer_index: HashMap<String, SkinCancer> = HashMap<String, SkinCancer>()
+        var skinCancer_index: HashMap<String, SkinCancer> = HashMap<String, SkinCancer>()
         
         fun createByPKSkinCancer(idx: String): SkinCancer {
-            var result: SkinCancer? = SkinCancer_index[idx]
+            var result: SkinCancer? = skinCancer_index[idx]
             if (result != null) { return result }
                   result = SkinCancer()
-                  SkinCancer_index.put(idx,result)
+                  skinCancer_index.put(idx,result)
                   result.id = idx
                   return result
         }
         
 		fun killSkinCancer(idx: String?) {
-            val rem = SkinCancer_index[idx] ?: return
+            val rem = skinCancer_index[idx] ?: return
             val remd = ArrayList<SkinCancer>()
             remd.add(rem)
-            SkinCancer_index.remove(idx)
-            SkinCancer_allInstances.removeAll(remd)
+            skinCancer_index.remove(idx)
+            skinCancer_allInstances.removeAll(remd)
         }        
     }
 
